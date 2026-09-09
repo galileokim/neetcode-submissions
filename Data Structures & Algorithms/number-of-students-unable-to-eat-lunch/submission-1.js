08e811335b0552bf38341275c9bea39a7ff3410b
Galileo Kim <97807671+galileokim@users.noreply.github.com>
2026-09-09 14:50:30 -0600

    Add: number-of-students-unable-to-eat-lunch - submission-1

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-1.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} students
+     * @param {number[]} sandwiches
+     * @return {number}
+     */
+    countStudents(students, sandwiches) {
+        
+        while (this.studentHasPref(students, sandwiches)) {
+            if (students[0] == sandwiches[0]) {
+                students.shift()
+                sandwiches.shift()
+            } else {
+                students.push(students.shift())
+            }
+        }
+
+        return students.length
+    }
+
+    studentHasPref(students, sandwiches) {
+        let std = new Set(students)
+
+        if (sandwiches.length == 0) {
+            return false
+        }
+
+        if (std.has(sandwiches[0])) {
+            return true
+        }
+        return false
+
+        // students = [0, 0] sandwiches = [1, 0, 0]
+    }
+}
