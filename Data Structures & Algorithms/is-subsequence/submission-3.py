99facc6779e65823fca5d3b9c6223088bd8c2871
Galileo Kim <97807671+galileokim@users.noreply.github.com>
2026-09-09 14:02:42 -0600

    Add: is-subsequence - submission-3

---
+++ Data Structures & Algorithms/is-subsequence/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        pos = 0
+
+        for c in t:
+            if c == s[pos]:
+                pos += 1
+        
+        if pos == len(s):
+            return True
+        
+        return False
+            
+
+        
