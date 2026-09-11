9a27a5617ec06c93ae7d24ffa42e01bbf874178f
Galileo Kim <97807671+galileokim@users.noreply.github.com>
2026-09-11 13:20:55 -0600

    Add: minimum-difference-between-highest-and-lowest-of-k-scores - submission-1

---
+++ Data Structures & Algorithms/minimum-difference-between-highest-and-lowest-of-k-scores/submission-1.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number}
+     */
+    minimumDifference(nums, k) {
+        // window size is length of array
+        nums.sort((a,b) => a - b)
+        console.log(nums)
+        if (nums.length < k) {
+            return nums[r] - nums[l]
+        }
+
+        let l = 0
+        let r = k - 1
+        let min = nums[r] - nums[l]
+
+        while (r < nums.length) {
+            min = Math.min(nums[r] - nums[l], min)
+            l += 1
+            r += 1
+        }
+
+        return min
+    }
+}
