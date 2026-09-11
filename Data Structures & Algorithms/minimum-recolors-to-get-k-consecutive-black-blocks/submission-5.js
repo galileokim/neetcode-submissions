b920c314097b6c515270ae2113ce60005b811951
Galileo Kim <97807671+galileokim@users.noreply.github.com>
2026-09-11 13:48:04 -0600

    Add: minimum-recolors-to-get-k-consecutive-black-blocks - submission-5

---
+++ Data Structures & Algorithms/minimum-recolors-to-get-k-consecutive-black-blocks/submission-5.js
@@ -0,0 +1,52 @@
+class Solution {
+    /**
+     * @param {string} blocks
+     * @param {number} k
+     * @return {number}
+     */
+    minimumRecolors(blocks, k) {
+        let whites = 0
+        if (blocks.length < k) {
+            for (let i = 0; i < blocks.length; i++) {
+                if (blocks[i] == "W") {
+                    whites++
+            }
+            return whites
+        }
+        }
+
+        let l = 0
+        let r = k - 1
+        console.log(blocks.slice(l,k))
+        
+        let min = Infinity
+
+        for (let i = 0; i < r; i++) {
+            if (blocks[i] == "W") {
+                whites++
+            }
+        }
+
+        console.log(whites)
+        min = Math.min(whites, min)
+        console.log(min)
+
+        while (r < blocks.length) {
+            min = Math.min(whites, min)
+
+            if (blocks[l] == "W") {
+                whites--
+            }
+
+            l += 1
+
+            r += 1
+
+            if (blocks[r] == "W") {
+                whites++
+            }
+        }
+
+        return min
+    }
+}
